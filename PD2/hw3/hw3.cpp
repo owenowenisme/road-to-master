c60968219678f9a1ab591f75221708df07d382e4
owenowenisme <mses010108@gmail.com>
2023-05-10 00:46:47 +0800

    some hw update

--- PD2/hw3/hw3.cpp
+++ PD2/hw3/hw3.cpp
@@ -1,55 +1,6 @@
 #include <bits/stdc++.h>
-#include <omp.h>
-#pragma omp parallel num_threads(16)
-#pragma GCC optimize(3)
-#pragma GCC target("avx")
-#pragma GCC optimize("Ofast")
-#pragma GCC optimize("inline")
-#pragma GCC optimize("-fgcse")
-#pragma GCC optimize("-fgcse-lm")
-#pragma GCC optimize("-fipa-sra")
-#pragma GCC optimize("-ftree-pre")
-#pragma GCC optimize("-ftree-vrp")
-#pragma GCC optimize("-fpeephole2")
-#pragma GCC optimize("-ffast-math")
-#pragma GCC optimize("-fsched-spec")
-#pragma GCC optimize("unroll-loops")
-#pragma GCC optimize("-falign-jumps")
-#pragma GCC optimize("-falign-loops")
-#pragma GCC optimize("-falign-labels")
-#pragma GCC optimize("-fdevirtualize")
-#pragma GCC optimize("-fcaller-saves")
-#pragma GCC optimize("-fcrossjumping")
-#pragma GCC optimize("-fthread-jumps")
-#pragma GCC optimize("-funroll-loops")
-#pragma GCC optimize("-fwhole-program")
-#pragma GCC optimize("-freorder-blocks")
-#pragma GCC optimize("-fschedule-insns")
-#pragma GCC optimize("inline-functions")
-#pragma GCC optimize("-ftree-tail-merge")
-#pragma GCC optimize("-fschedule-insns2")
-#pragma GCC optimize("-fstrict-aliasing")
-#pragma GCC optimize("-fstrict-overflow")
-#pragma GCC optimize("-falign-functions")
-#pragma GCC optimize("-fcse-skip-blocks")
-#pragma GCC optimize("-fcse-follow-jumps")
-#pragma GCC optimize("-fsched-interblock")
-#pragma GCC optimize("-fpartial-inlining")
-#pragma GCC optimize("no-stack-protector")
-#pragma GCC optimize("-freorder-functions")
-#pragma GCC optimize("-findirect-inlining")
-#pragma GCC optimize("-fhoist-adjacent-loads")
-#pragma GCC optimize("-frerun-cse-after-loop")
-#pragma GCC optimize("inline-small-functions")
-#pragma GCC optimize("-finline-small-functions")
-#pragma GCC optimize("-ftree-switch-conversion")
-#pragma GCC optimize("-foptimize-sibling-calls")
-#pragma GCC optimize("-fexpensive-optimizations")
-#pragma GCC optimize("-funsafe-loop-optimizations")
-#pragma GCC optimize("inline-functions-called-once")
-#pragma GCC optimize("-fdelete-null-pointer-checks")
 #define ac ios_base::sync_with_stdio(false),cin.tie(0)
-#define int unsigned int
+#define int unsigned long long
 using namespace std;
 struct info{
     int id=0;
@@ -67,19 +18,22 @@ bool cmp(info a,info b){
     return a.id<b.id;
 }
 typedef info info;
+//int now
 void solve(char* filename){
+    
     //info data[100000];
     info nothing;
-    vector<info>data;
-    freopen(filename,"r",stdin);
+     vector<info>data;
+    //freopen(filename,"r",stdin);
+    ifstream in(filename);
     string s;
     stringstream ss;
-    int idx=0,id=0,arr[3];
-    while(getline(cin,s)){
+    register int idx=0,id=0,arr[3];
+    while(in>>s){
         ss<<s; id=0;
         while(getline(ss,s,',')){
             if(id==1){
-                if(s=="sign-in")
+                if(s[5]=='i')
                     arr[1]=0;
                 else 
                     arr[1]=1;
@@ -93,7 +47,7 @@ void solve(char* filename){
         nothing.sign=arr[1];
         nothing.time=arr[2];
         arr[2]*=0.0001;
-        nothing.date=arr[2]%10000;
+        nothing.date=arr[2];
         idx++;
 	data.push_back(nothing);
         ss.clear();
@@ -101,34 +55,29 @@ void solve(char* filename){
     
 
     sort(data.begin(),data.end(),cmp);
-    int current_id=data[0].id,current_overload=0,current_forget=0;
-    stack<info>st;
-    st.push(data[0]);
-    #pragma omp parallel for
-    for(int i=0;i<idx;++i){
-	    //cout<<data[i].id<<'\n';
+    register int current_id=data[0].id,current_overload=0;
+    register stack<int>st;
+    register stack<int>emp;
+    st.push(0);
+//    #pragma omp parallel for
+    for(register int i=0;i<idx;++i){
             if(current_id!=data[i].id){
                 cout<<current_id<<','<<current_overload<<','<<st.size()<<'\n';
-                while(!st.empty())st.pop();
-                st.push(data[i]);
-                current_id=data[i].id,current_overload=0,current_forget=0;
+                st=emp;
+		st.push(i);
+                current_id=data[i].id,current_overload=0;
             }else{
-                if(st.empty()){st.push(data[i]);continue;}
-                if(st.top().date==data[i].date){
-                    if(st.top().sign==0&&data[i].sign==1){
-                        if(data[i].time-st.top().time>800){
-			asm volatile(
-			"add $1,%0;"
-			:[x] "+&r" (current_overload)
-			:		
-			);
-			//current_overload++;
+                if(st.empty()){st.push(i);continue;}
+                if(data[st.top()].date==data[i].date){
+                    if(data[st.top()].sign==0&&data[i].sign==1){
+                        if(data[i].time-data[st.top()].time>800){
+			current_overload++;
                         }
                         st.pop();
                     }
                 }
                 else{
-                        st.push(data[i]);
+                        st.push(i);
                 }
             }
         
@@ -138,6 +87,7 @@ void solve(char* filename){
     cout<<current_id<<','<<current_overload<<','<<st.size()<<'\n';
 }
 signed main(int argc,char *argv[]){ac;
+    //now =clock();
     solve(argv[1]);
     return 0;
 }
